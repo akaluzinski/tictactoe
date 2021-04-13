76c1819ff698826dce7bd2a4286b35352bb779fa
akaluzinski <adrian@kaluzinski.eu>
2021-04-13 17:20:57 +0200

    Display winner

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-        <button className="square"
-                onClick={() => { this.props.onClick() }}>
-          {this.props.value}
-        </button>
-    );
-  }
+function Square({onClick, value}) {
+  return <button className="square" onClick={onClick}>
+    {value}
+  </button>;
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      isXNext: true
     };
   }
 
@@ -29,12 +25,24 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares })
+    squares[i] = this.getNextToken();
+    const isXNext = !this.state.isXNext;
+    this.setState({ squares, isXNext })
+  }
+
+  getNextToken() {
+    return this.state.isXNext ? 'X' : 'O';
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    if(winner) {
+      status =  `${winner} WINS!`;
+    } else{
+      status = `Next player: ${this.getNextToken()}`
+    }
 
     return (
         <div>
@@ -81,3 +89,24 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
