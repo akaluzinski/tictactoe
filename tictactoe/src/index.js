64421286f542af57423462713dbed46954a211f2
akaluzinski <adrian@kaluzinski.eu>
2021-04-13 17:31:58 +0200

    Refactor calculateWinner()

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -100,11 +100,10 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+
+  return lines.map(([a, b, c]) => {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-  }
-  return null;
+  }).find(w => w);
 }
