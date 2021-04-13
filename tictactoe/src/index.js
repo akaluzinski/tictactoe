adb36ea585752cf4236aa75bb27c37503230becb
akaluzinski <adrian@kaluzinski.eu>
2021-04-13 17:57:25 +0200

    pull-up score and add history

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -9,29 +9,57 @@ function Square({onClick, value}) {
 }
 
 class Board extends React.Component {
+  renderSquare(i) {
+    return <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+    />;
+  }
+  render() {
+    return <div>
+      <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+      </div>
+    </div>;
+  }
+}
+
+class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
-      isXNext: true
-    };
-  }
-
-  renderSquare(i) {
-    return <Square
-        onClick={() => this.handleClick(i)}
-        value={this.state.squares[i]} />;
+      history: [{
+        squares: Array(9).fill(null),
+        isXNext: true
+      }]
+    }
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
 
     squares[i] = this.getNextToken();
-    const isXNext = !this.state.isXNext;
-    this.setState({ squares, isXNext })
+    this.setState({
+      history: history.concat([{squares}]),
+      isXNext: !this.state.isXNext,
+    });
   }
 
   getNextToken() {
@@ -39,41 +67,20 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
     const status = winner ? `${winner} WINS!` : `Next player: ${this.getNextToken()}`;
 
     return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i)}  />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
